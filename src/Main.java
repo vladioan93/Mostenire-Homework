b8246de37fab23de6a336b0c0918b9052577280c
vlad <prioteasa.vlad@gmail.com>
2020-03-16 16:52:17 +0200

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myVehicle = new Vehicle("d213f423gf", 4,"b12pvi");
+        myVehicle.addFuel(23);
+        myVehicle.goTo(13,24);
+        myVehicle.printInfo();
+    }
+}
+
---
+++ src/Vehicle.java
@@ -0,0 +1,39 @@
+public class Vehicle {
+    private final String serialNumber;
+    private int noPersons;
+    private String name;
+
+    public Vehicle(String serialNumber, int noPersons) {
+        this.serialNumber = serialNumber;
+        this.noPersons = noPersons;
+    }
+
+    public Vehicle(String serialNumber, int noPersons, String name) {
+        this.serialNumber = serialNumber;
+        this.noPersons = noPersons;
+        this.name = name;
+    }
+
+    public final String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public boolean goTo(double positionX, double positionY){
+        System.out.println("Error: unknown vehicle cannot move...");
+
+        return false;
+    }
+
+    public boolean addFuel(double amount){
+        System.out.println("Error: unknown vehicle cannot move...");
+
+        return false;
+    }
+
+    public void printInfo(){
+        System.out.println("vehicle properties: " +
+                "\n\t- serial number: " + this.serialNumber +
+                "\n\t- capacity: " + this.noPersons +
+                "\n\t- name: " + this.name);
+    }
+}
